ca57ed2ac4fd061e3fa1847f5ca66b0e35c38c44
Harshita <agrawalharshita743@gmail.com>
2024-08-12 17:20:58 +0530

    second commit

---
+++ file1.js
@@ -0,0 +1,71 @@
+// let a=10;
+// document.write(a, "<br/>")
+// let b=5;
+// document.write(b, "<br/>")
+// let sum =a+b;
+// document.write(sum, "<br/>")
+// let difference = a-b;
+// document.write(difference, "<br/>")
+// let multiplication =a*b;
+// document.write(multiplication, "<br/>")
+// let quotient =a/b;
+// document.write(quotient, "<br/>")
+// let remainder =a%b;
+// document.write(remainder, "<br/>")
+// let isEqual =(a==b);
+// document.write(isEqual, "<br/>")
+// let isNotEqual =(a!=b);
+// document.write(isNotEqual, "<br/>")
+// let isGreater =(a>b);
+// document.write(isGreater, "<br/>")
+// let fname="john";
+// document.write("hello", fname,"<br/>")
+// let age = "38"
+// document.write("Age",age, "<br/>")
+// let isStudent = true;
+// document.write(`Student - ${isStudent? "Yes" : "No"} <br>`)
+// let emptyValue = null;
+// document.write(`About us ${emptyValue || "write something"} <br>`)
+// let uninitialized;
+// document.write(!uninitialized && "fill details completely")
+
+//----------------------------------------------------------------------
+
+//  let Age = 18;
+//  if (Age>17) {
+//      document.write("eligible for vote");
+//  } else {
+//      document.write("not eligible");
+//  }
+// document.write("<br>")
+// //or
+// document.write(age> 17 ? "Eligible for vote":"not eligible")
+
+
+
+//----------------------------------------------------------------------
+
+
+// let number = 10;
+// let n = prompt("enter number");
+// if  (number>0){
+//     console.log("the number is positive.");
+// }else if (number < 0){
+//     console.log("the number is negative.");
+// }else {
+//     console.log("the number is zero.");
+// } 
+
+//---------------------------------------------------------------------
+
+
+let fruit = prompt("Enter fruit name=");
+switch (fruit) {
+    case "apple":
+        console.log("you choose an apple.");
+        break;
+    case "banana":
+        console.log("you choose banana");
+        break;
+        
+}
