16154fdd1992991cd3e609643c9ed32117dba4c9
Harshita <agrawalharshita743@gmail.com>
2024-08-13 16:03:31 +0530

    13-08-2024

--- file1.js
+++ file1.js
@@ -59,13 +59,113 @@
 //---------------------------------------------------------------------
 
 
-let fruit = prompt("Enter fruit name=");
-switch (fruit) {
-    case "apple":
-        console.log("you choose an apple.");
+// let fruit = prompt("Enter fruit name:");
+// switch (fruit) {
+//     case "apple":
+//         console.log("you choose an apple.");
+//         break;
+//     case "banana":
+//         console.log("you choose banana");
+//         break;
+//       default:
+//             console.log("Unknown fruit.");
+// }
+
+
+//----------------------------------------------------------------------
+
+let a = prompt("enter the number:");
+let b = prompt("enter the number:");
+var ch = prompt("enter youe choice");
+switch (ch) {
+      case "+":
+        console.log(parseInt(a)+parseInt(b));
         break;
-    case "banana":
-        console.log("you choose banana");
+      case "-":
+        console.log(parseInt(a)-parseInt(b));
         break;
-        
+      case "*":
+        console.log(parseInt(a)*parseInt(b));
+        break;
+      case "/":
+        console.log(parseInt(a)/parseInt(b));
+        break;
+      case "%":
+        console.log(parseInt(a)%parseInt(b));
+        break;
+      default:
+            console.log("Unknown number.");   
 }
+//------------------------------------------------------------------------
+
+// simple function
+// function greeting(){
+//   document.write("happy indepedent day")
+// }
+
+// greeting()
+// 6
+
+// // function Expression
+
+// let day = function(){
+//   document.write("Have a nice day<br>")
+// }
+// day()
+// day()
+
+// // arrow function
+
+// let msg = () => {
+//   document.write("Happy birthday<br>")
+// }
+// msg()
+// msg()
+
+// // iife
+
+
+
+// (function(){
+//     document.write("This is IIFE function");
+// }());
+
+//----------------------------------------------------------------------
+
+// function greeting() {
+//   document.write("Welcome friends")
+//   document.getElementById("demo").innerHTML = "HAVE A NICE DAY"
+// }
+// greeting()
+
+//-----------------------------------------------------------------------
+
+// function Morning(){
+//   document.getElementById("demo").innerHTML = "Morning"
+//   document.getElementById("mode").style.background = "blue"
+//   document.getElementById("mode").style.color = "black"
+// }
+// morning()
+
+// function Evening(){
+//   document.getElementById("demo").innerHTML="Evening"
+//   document.getElementById("mode").style.background = "skyblue"
+//   document.getElementById("mode").style.color = "black"
+// }
+// Evening()
+
+// function Afternoon(){
+//   document.getElementById("demo").innerHTML="Afternoon"
+//   document.getElementById("mode").style.background = "#f9f5a2"
+//   document.getElementById("mode").style.color = "black"
+// }
+// Afternoon()
+
+// function Night(){
+//   document.getElementById("demo").innerHTML="Night"
+//   document.getElementById("mode").style.background = "black"
+//   document.getElementById("mode").style.color = "white"
+// }
+// Night()
+
+
